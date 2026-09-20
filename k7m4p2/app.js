e0a26aee85dfc61cac1968971d50228a3d349512
WCbeardown <99541694+WCbeardown@users.noreply.github.com>
2026-09-20 22:01:49 +0900

    Update app.js

--- k7m4p2/app.js
+++ k7m4p2/app.js
@@ -115,11 +115,36 @@ function renderRecentStats(data) {
     if (row.input_data && typeof row.input_data === "object") {
       inputText = Object.entries(row.input_data)
         .map(([key, value]) => {
-          if (Array.isArray(value)) {
-            value = value.join(", ");
-          } else if (typeof value === "object" && value !== null) {
-            value = JSON.stringify(value);
-          }
+    if (Array.isArray(value)) {
+    
+      value = value.map(item => {
+    
+        // habikino_champions の選択項目
+        // 例：
+        // { part: "2", rank: "1", className: "B" }
+        // → B21
+        if (
+          item &&
+          typeof item === "object" &&
+          item.className &&
+          item.part &&
+          item.rank
+        ) {
+          return `${item.className}${item.part}${item.rank}`;
+        }
+    
+        return String(item);
+    
+      }).join(", ");
+    
+    } else if (
+      typeof value === "object" &&
+      value !== null
+    ) {
+    
+      value = JSON.stringify(value);
+    
+    }
 
           return `${key}: ${value}`;
         })
